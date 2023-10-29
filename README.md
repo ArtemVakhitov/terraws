9496162e4ffdb503329bfcf85883771633dd4adf
Artem Vakhitov <artem.vakhitov@gmail.com>
2023-10-29 14:55:20 +0600

    Initial template version and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# terraws
+# Terraws
+
+This Terraform template creates an AWS instance of type t2.xxx, given the required amount of RAM (in GiB) and vCPU. If too little RAM is requested for this number of CPUs, it instead creates a t2 instance with more RAM to match the CPU number, and vice versa. Any amount of RAM less than 1 GiB is interpreted as 0.5 GiB (t2.nano).
---
+++ terraws.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
+}
+
+provider "aws" {
+ region = "us-east-1"
+}
+
+variable "cpu" {
+  type = number
+  default = 1
+}
+
+variable "ram" {
+  type = number
+  default = 2
+}
+
+locals {
+  # Log metric to use as list index for RAM amount
+  lram = (var.ram < 1) ? 0 : min(ceil(log(2, var.ram), 5) + 1
+  # Log metric to use as list index for CPU number
+  lcpu = min(ceil(log(2, var.cpu), 3)
+  # No. of CPUs for that amount of RAM in standard instance types
+  cpu_for_lram = [1, 1, 1, 2, 2, 4, 8]
+  # Instance types grouped by CPU number
+  instance_types = [["t2.nano", "t2.micro", "t2.small"], ["t2.medium", "t2.large"], ["t2.xlarge"], ["t2.2xlarge"]]
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0261755bbcb8c4a84"
+  # If CPU requirement <= standard no. of CPUs for that RAM amount, select instance type based on RAM
+  # If CPU requirement > standard no. of CPUs for that RAM amount, select instance type with that many CPUs and minimum RAM
+  instance_type = (var.cpu <= cpu_for_lram[lram]) ? flatten(instance_types)[lram] : instance_types[lcpu][0])]
+}
